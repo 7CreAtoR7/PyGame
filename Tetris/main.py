3a6e1b08599f00c3a3fdfb96522168beb257085c
Daniil <danielsedovzzz@gmail.com>
2021-01-09 02:28:49 +0300

    Готовая версия без спрайтов

--- Tetris/main.py
+++ Tetris/main.py
@@ -1,6 +1,174 @@
+from copy import deepcopy
 import pygame
 import random
 import sqlite3
+
+
+class Figure:
+    def __init__(self, tetris, position, blocks):
+        self.tetris = tetris
+        for idx in range(len(blocks)):
+            blocks[idx][0] += position[0]
+            blocks[idx][1] += position[1]
+            row, col = blocks[idx]
+            if tetris.board[row][col] == 1:
+                return False
+            tetris.board[row][col] = 1
+        self.blocks = blocks
+
+    def get_most_down(self):
+        return max([row for row, col in self.blocks])
+
+    def get_most_left(self):
+        return min([col for row, col in self.blocks])
+
+    def get_most_right(self):
+        return max([col for row, col in self.blocks])
+
+    def move_down(self):
+        if self.get_most_down() + 1 >= self.tetris.height:
+            return False
+
+        blocks = deepcopy(self.blocks)
+        board = deepcopy(tetris.board)
+
+        for idx in range(len(blocks)):
+            row, col = blocks[idx]
+            board[row][col] = 0
+            blocks[idx] = (row + 1, col)
+
+        for row, col in blocks:
+            if board[row][col] == 1:
+                break
+            board[row][col] = 1
+        else:
+            self.blocks = blocks
+            tetris.board = board
+            return True
+        return False
+
+    def move_left(self):
+        if self.get_most_down() + 1 >= self.tetris.height or \
+           self.get_most_left() <= 0:
+            return False
+
+        blocks = deepcopy(self.blocks)
+        board = deepcopy(tetris.board)
+
+        for idx in range(len(blocks)):
+            row, col = blocks[idx]
+            board[row][col] = 0
+            blocks[idx] = (row, col - 1)
+
+        for row, col in blocks:
+            if board[row][col] == 1:
+                break
+            board[row][col] = 1
+        else:
+            self.blocks = blocks
+            tetris.board = board
+            return True
+        return False
+
+    def move_right(self):
+        if self.get_most_down() + 1 >= self.tetris.height or \
+           self.get_most_right() + 1 >= self.tetris.width:
+            return False
+
+        blocks = deepcopy(self.blocks)
+        board = deepcopy(tetris.board)
+
+        for idx in range(len(blocks)):
+            row, col = blocks[idx]
+            board[row][col] = 0
+            blocks[idx] = (row, col + 1)
+
+        for row, col in blocks:
+            if board[row][col] == 1:
+                break
+            board[row][col] = 1
+        else:
+            self.blocks = blocks
+            tetris.board = board
+            return True
+        return False
+
+
+class Hero(Figure):
+    def __init__(self, tetris, position):
+        blocks = [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [0, 3]
+        ]
+        super().__init__(tetris, position, blocks)
+
+
+class Smashboy(Figure):
+    def __init__(self, tetris, position):
+        blocks = [
+            [0, 0],
+            [0, 1],
+            [1, 0],
+            [1, 1]
+        ]
+        super().__init__(tetris, position, blocks)
+
+
+class Teewee(Figure):
+    def __init__(self, tetris, position):
+        blocks = [
+            [0, 1],
+            [1, 0],
+            [1, 1],
+            [1, 2]
+        ]
+        super().__init__(tetris, position, blocks)
+
+
+class OrangeRicky(Figure):
+    def __init__(self, tetris, position):
+        blocks = [
+            [0, 2],
+            [1, 0],
+            [1, 1],
+            [1, 2]
+        ]
+        super().__init__(tetris, position, blocks)
+
+
+class BlueRicky(Figure):
+    def __init__(self, tetris, position):
+        blocks = [
+            [0, 0],
+            [1, 0],
+            [1, 1],
+            [1, 2]
+        ]
+        super().__init__(tetris, position, blocks)
+
+
+class Cleveland(Figure):
+    def __init__(self, tetris, position):
+        blocks = [
+            [0, 0],
+            [0, 1],
+            [1, 1],
+            [1, 2]
+        ]
+        super().__init__(tetris, position, blocks)
+
+
+class RhodeIsland(Figure):
+    def __init__(self, tetris, position):
+        blocks = [
+            [0, 1],
+            [0, 2],
+            [1, 0],
+            [1, 1]
+        ]
+        super().__init__(tetris, position, blocks)
 
 
 class Tetris:
@@ -8,50 +176,25 @@ class Tetris:
         self.width = width
         self.height = height
         self.board = [[0] * width for _ in range(height)]
-        self.current_figure = (None, None)
+        self.current_figure = None
         self.cell_size = cell_size
         self.score = 0
 
-    def add_figure(self, figure_type=None):
-        col = random.randint(0, self.width - 1)
-        self.current_figure = (0, col)
-        self.board[0][col] = 1
-
-    def move_figure(self, direction='down'):
-        row, col = self.current_figure
-        if row is None:
-            return
-
-        if direction == 'down':
-            if row + 1 >= self.height:
-                return
-            self.current_figure = (row + 1, col)
-            self.board[row][col] = 0
-            self.board[row + 1][col] = 1
-        elif direction == 'left':
-            if row + 1 == self.height or col - 1 < 0:
-                return
-            self.current_figure = (row, col - 1)
-            self.board[row][col] = 0
-            self.board[row][col - 1] = 1
-        elif direction == 'right':
-            if row + 1 == self.height or col + 1 >= self.width:
-                return
-            self.current_figure = (row, col + 1)
-            self.board[row][col] = 0
-            self.board[row][col + 1] = 1
+    def add_figure(self, figure):
+        col = random.randint(3, self.width - 4)
+        self.current_figure = figure(self, (0, col))
+        if self.current_figure == False:
+            exit()  # TODO: Game over!
 
     def tick(self):
-        row, col = self.current_figure
-        if row is None:
-            self.add_figure()
+        figure = self.current_figure
+        if figure is None:
+            self.add_figure(next(FIGURES_SEQUENCE))
         else:
-            if row + 1 == self.height:
-                self.current_figure = (None, None)
-            elif self.board[row + 1][col]:
-                self.current_figure = (None, None)
-            else:
-                self.move_figure('down')
+            if figure.get_most_down() == self.height:
+                self.current_figure = None
+            elif figure.move_down() == False:
+                self.current_figure = None
 
             if all(self.board[-1]):
                 self.board[-1] = [0] * self.width
@@ -69,7 +212,22 @@ class Tetris:
                                  not bool(self.board[row][col]))
 
 
+def figures_sequence(figures):
+    figures_copy = figures[:]
+    while True:
+        figure = random.choice(figures_copy)
+        figures_copy = figures[:]
+        figures_copy.remove(figure)
+        yield figure
+
+
 if __name__ == '__main__':
+    DEFAULT_SPEED = 10
+    INCREASED_SPEED = 50
+    FIGURES = [Hero, Smashboy, Teewee, OrangeRicky,
+               BlueRicky, Cleveland, RhodeIsland]
+    FIGURES_SEQUENCE = figures_sequence(FIGURES)
+
     con = sqlite3.connect('database.sqlite')
     cur = con.cursor()
 
@@ -79,10 +237,7 @@ if __name__ == '__main__':
     )""")
 
     tetris = Tetris(10, 15, 30)
-    tetris.add_figure()
-
-    DEFAULT_SPEED = 10
-    INCREASED_SPEED = 50
+    tetris.add_figure(next(FIGURES_SEQUENCE))
 
     pygame.init()
     pygame.display.set_caption('Тетрис')
@@ -93,6 +248,7 @@ if __name__ == '__main__':
     speed = DEFAULT_SPEED
     current_tick = 0
     current_key = None
+    next_figure = None
 
     running = True
     while running:
@@ -110,14 +266,15 @@ if __name__ == '__main__':
                 if event.key in [97, 100, 115]:
                     current_key = None
 
-        if current_key == 97:
-            tetris.move_figure('left')
-        elif current_key == 100:
-            tetris.move_figure('right')
-        elif current_key == 115:  # pressed S
-            speed = INCREASED_SPEED
-        else:
-            speed = DEFAULT_SPEED
+        if tetris.current_figure:
+            if current_key == 97:
+                tetris.current_figure.move_left()
+            elif current_key == 100:
+                tetris.current_figure.move_right()
+            elif current_key == 115:  # pressed S
+                speed = INCREASED_SPEED
+            else:
+                speed = DEFAULT_SPEED
 
         if current_tick >= 50:
             tetris.tick()
