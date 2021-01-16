4674c7ccce97d90ed00affcfc6c9cd2ad9d9bfb4
Ilya <48677105+7CreAtoR7@users.noreply.github.com>
2021-01-17 00:42:15 +0300

    Tetris game

--- Tetris/Tetris.py
+++ Tetris/Tetris.py
@@ -128,11 +128,73 @@ class Figure:
 
         con.commit()
 
+    def finish_game(self):
+        pygame.init()
+        size = width, height = 1400, 900
+        screen = pygame.display.set_mode(size)
+        pygame.display.set_caption('Тетрис')
+
+        def load_image(name):
+            fullname = os.path.join('data', name)
+            # если файл не существует, то выходим
+            if not os.path.isfile(fullname):
+                print(f"Файл с изображением '{fullname}' не найден")
+                sys.exit()
+            image = pygame.image.load(fullname)
+            return image
+
+        class Car(pygame.sprite.Sprite):
+            image = load_image("game over.jpg")  # это фото game over
+
+            def __init__(self, *group):
+                super().__init__(*group)
+                self.image = Car.image
+                self.rect = self.image.get_rect()
+                self.rect.x = -600
+                self.rect.y = 212
+
+            def update(self, step):
+                if self.rect.x >= 300:
+                    self.rect.x = 300  # ценрт по иксу
+                    self.rect.y = 212  # центр по игреку
+                else:
+                    self.rect = self.rect.move(step, 0)
+
+        all_sprites = pygame.sprite.Group()
+        sprite = pygame.sprite.Sprite()
+        sprite.image = load_image("background.jpg")  # это звездный фон
+        sprite.rect = sprite.image.get_rect()
+        all_sprites.add(sprite)
+
+        for _ in range(1):
+            Car(all_sprites)
+
+        running = True
+        clock = pygame.time.Clock()
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            screen.fill((0, 0, 255))
+            # в главном игровом цикле
+            all_sprites.draw(screen)
+
+            clock.tick(500)  # задержка
+            all_sprites.update(3)  # При двух - в 2 раза быстрее обычного, при трех - в три и т.д.
+            pygame.display.flip()
+        pygame.quit()
+
+
+
+
+
     def get_most_down(self):
         try:
             return max([row for row, col in self.blocks])
         except:
             self.update_result()
+            self.finish_game()
             exit() # GAME OVER
 
     def get_most_left(self):
@@ -140,6 +202,7 @@ class Figure:
             return min([col for row, col in self.blocks])
         except:
             self.update_result()
+            self.finish_game()
             exit() # GAME OVER
 
     def get_most_right(self):
@@ -147,6 +210,7 @@ class Figure:
             return max([col for row, col in self.blocks])
         except:
             self.update_result()
+            self.finish_game()
             exit() # GAME OVER
 
     def move_down(self):
